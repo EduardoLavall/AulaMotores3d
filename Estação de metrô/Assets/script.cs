6807d1e50c54815bc032919af9f65829db3d0f49
EduardoLavall <eduardolavalldias@gmail.com>
2024-03-04 21:45:59 -0300

    First commit

---
+++ Estação de metrô/Assets/script.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Print objeto");
+        Debug.Log("debug.log");
+        Debug.LogWarning("log warning");
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
